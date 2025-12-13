e9b1df2c44492c4a48a88d0275581e63f45d6424
samdwivedi <sameerdwivedi2005@gmail.com>
2025-12-13 20:49:38 +0530

    uploaded new files

--- src/exception.py
+++ src/exception.py
@@ -1,24 +1,30 @@
 import sys
 from src.logger import logging
 
-def error_message_detail(error,error_detail:sys):
-    _,_,exc_tb=error_detail.exc_info()
-    file_name=exc_tb.tb_frame.f_code.co_filename
-    error_message="Error occured in python script name [{0}] line number [{1}] error message[{2}]".format(
-     file_name,exc_tb.tb_lineno,str(error))
+
+def error_message_detail(error, error_detail: sys):
+    _, _, exc_tb = error_detail.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    error_message = "Error occured in python script name [{0}] line number [{1}] error message[{2}]".format(
+        file_name, exc_tb.tb_lineno, str(error)
+    )
 
     return error_message
 
-    
 
 class CustomException(Exception):
-    def __init__(self,error_message,error_detail:sys):
+    def __init__(self, error_message, error_detail: sys):
         super().__init__(error_message)
-        self.error_message=error_message_detail(error_message,error_detail=error_detail)
-    
+        self.error_message = error_message_detail(error_message, error_detail)
+        logging.error(self.error_message)  # <--- LOG THE ERROR
+
     def __str__(self):
         return self.error_message
-    
 
 
-        
+if __name__ == "__main__":
+    try:
+        a = 1 / 0
+    except Exception as e:
+        logging.info("dividing error message")
+        raise CustomException(e, sys)
